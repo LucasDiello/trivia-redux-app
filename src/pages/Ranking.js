cac55917209b1a6ee35eed55ac03695b8959fa81
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-06-26 21:14:03 -0300

    feat: add button in Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking Title</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          {' '}
+          Ínicio
+          {' '}
+
+        </button>
       </div>
     );
   }
 }
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
